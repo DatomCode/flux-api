3691a4111639f0ca3bd71145f67539d2e9f0b6b4
Datom-code <enochgbadebo007@gmail.com>
2026-04-07 16:53:43 +0100

    connected supabase for database and wrote the user model

--- backend/fluxapi/models.py
+++ backend/fluxapi/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    role = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
