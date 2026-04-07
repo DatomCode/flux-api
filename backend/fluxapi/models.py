b59d1d344114600409023f3eb7103fd1e58e5c3a
Datom-code <enochgbadebo007@gmail.com>
2026-04-07 18:08:51 +0100

    made changes to models and fixed leaked .env file

--- backend/fluxapi/models.py
+++ backend/fluxapi/models.py
@@ -4,13 +4,24 @@ from django.db import models
 
 
 class User(models.Model):
+
+    ROLE = {
+        'admin': 'Admin',
+        'customer': 'Customer',
+        'rider': 'Rider',
+        'sender': 'Sender'
+    }
+
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
     email = models.EmailField()
-    role = models.CharField(max_length=50)
+    role = models.CharField(max_length=50, choices=ROLE.items())
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
+
+
+
