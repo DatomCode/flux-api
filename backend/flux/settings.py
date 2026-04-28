329d232583bc6bc85d94ddc5a89fdf1e3dc6fb19
Datom-code <enochgbadebo007@gmail.com>
2026-04-28 20:39:50 +0100

    Add Celery configuration and initialize app in celery.py

--- backend/flux/settings.py
+++ backend/flux/settings.py
@@ -157,3 +157,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Lagos'
