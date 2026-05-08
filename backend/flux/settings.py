3b0ac9b570dbcd8516b88a9c813a97becbff2057
Datom-code <enochgbadebo007@gmail.com>
2026-05-08 14:46:43 +0100

    Add AJAX form handling and prettify JavaScript for code highlighting
    - Introduced `load-ajax-form.js` to enable AJAX form submission on document ready.
    - Added `prettify-min.js` for enhanced code formatting and syntax highlighting in the application.

--- backend/flux/settings.py
+++ backend/flux/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,7 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
@@ -203,3 +206,5 @@ CACHES = {
         'LOCATION': REDIS_URL,
     }
 }
+
+
