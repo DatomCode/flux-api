2235037dfef56c162dd2747109f8817667f001b7
Datom-code <enochgbadebo007@gmail.com>
2026-08-16 12:02:48 -0400

    Register models in Django admin

--- backend/fluxapi/admin.py
+++ backend/fluxapi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserProfile, Address, RiderProfile, CustomerProfile, Company, Order, DeliveryCode, DeliveryStatusLog
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Address)
+admin.site.register(RiderProfile)
+admin.site.register(CustomerProfile)
+admin.site.register(Company)
+admin.site.register(Order)
+admin.site.register(DeliveryCode)
+admin.site.register(DeliveryStatusLog)
